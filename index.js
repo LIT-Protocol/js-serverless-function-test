02dc8b2958f18df274b87edd3120aa6555d8308b
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-05-13 16:17:40 -0700

    comments

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const go = async () => {
   //   "https://ipfs.litgateway.com/ipfs/QmNiDrDnTiSo4y78qKwaZboq8KfT9Y3CRrnM7pfUmG1EFq"
   // ).then((response) => response.json());
   // console.log("data: " + JSON.stringify(data, null, 2));
-  const arr = [65, 65, 65];
+  const arr = [65, 65, 65]; // this is the string "AAA" for testing
   const sig = await Deno.core.opAsync("op_sign_ecdsa", arr);
   console.log("sig: ", sig);
 };

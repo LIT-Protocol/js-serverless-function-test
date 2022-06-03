4c76756dc177a118069dbd2659af973144c66f8b
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-06-02 23:48:36 -0700

    LitJsSdk.executeJs working well in helloworld.js

--- signAndCombine.js
+++ signAndCombine.js
@@ -10,6 +10,14 @@ import {
 } from "@ethersproject/bytes";
 import { recoverPublicKey, computePublicKey } from "@ethersproject/signing-key";
 
+const authSig = {
+  sig: "0x2bdede6164f56a601fc17a8a78327d28b54e87cf3fa20373fca1d73b804566736d76efe2dd79a4627870a50e66e1a9050ca333b6f98d9415d8bca424980611ca1c",
+  derivedVia: "web3.eth.personal.sign",
+  signedMessage:
+    "localhost wants you to sign in with your Ethereum account:\n0x9D1a5EC58232A894eBFcB5e466E3075b23101B89\n\nThis is a key for Partiful\n\nURI: https://localhost/login\nVersion: 1\nChain ID: 1\nNonce: 1LF00rraLO4f7ZSIt\nIssued At: 2022-06-03T05:59:09.959Z",
+  address: "0x9D1a5EC58232A894eBFcB5e466E3075b23101B89",
+};
+
 // import LitJsSdk from "lit-js-sdk";
 
 const testBlsSigning = async () => {
@@ -76,7 +84,7 @@ const testEcdsaSigning = async () => {
 
   for (let i = 0; i < 10; i++) {
     const url = `http://127.0.0.1:${basePort + i}/web/execute`;
-    promises.push(axios.post(url, { js_base64: encodedJs }));
+    promises.push(axios.post(url, { code: encodedJs, authSig }));
   }
 
   const responses = await Promise.all(promises);

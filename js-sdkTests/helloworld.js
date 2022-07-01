bb2fc76a0cc742cf5930009c9d30a365eb8e6fed
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-06-30 21:12:39 -0400

    add params to hello world test

--- js-sdkTests/helloworld.js
+++ js-sdkTests/helloworld.js
@@ -10,6 +10,54 @@ const go = async () => {
   const sigShare = await LitActions.signEcdsa({ toSign, keyId: 1, sigName: "sig1" });
 };
 
+// check the params we passed in:
+const correctParams = {
+  aString: "meow",
+  anInt: 42,
+  aFloat: 123.456,
+  anArray: [1, 2, 3, 4],
+  anArrayOfStrings: ["a", "b", "c", "d"],
+  anObject: { x: 1, y: 2 },
+  anObjectOfStrings: { x: "a", y: "b" },
+};
+
+// abort if any of these mismatch.  the signing won't happen.
+if (
+  aString !== correctParams.aString ||
+  anInt !== correctParams.anInt ||
+  aFloat !== correctParams.aFloat ||
+  JSON.stringify(anArray) !== JSON.stringify(correctParams.anArray) ||
+  JSON.stringify(anArrayOfStrings) !==
+    JSON.stringify(correctParams.anArrayOfStrings) ||
+  JSON.stringify(anObject) !== JSON.stringify(correctParams.anObject) ||
+  JSON.stringify(anObjectOfStrings) !==
+    JSON.stringify(correctParams.anObjectOfStrings)
+) {
+  // noooo
+  console.log("------------- HEY!  Notice this! -------------");
+  console.log("One of the params passed in is not matching");
+  console.log("correctParams", JSON.stringify(correctParams));
+  console.log(
+    "Go and figure out which one below isn't matching correctParams above to debug this"
+  );
+  console.log("aString: ", aString);
+  console.log("anInt: ", anInt);
+  console.log("aFloat: ", aFloat);
+  console.log("anArray: ", anArray);
+  console.log("anArrayOfStrings: ", anArrayOfStrings);
+  console.log("anObject: ", anObject),
+    console.log("anObjectOfStrings: ", anObjectOfStrings);
+  console.log(
+    "------------- EXITING LIT ACTION due to above error -------------"
+  );
+  process.exit(0);
+} else {
+  console.log('all the params match!')
+}
+
+
+
+
 go();
 `;
 
@@ -39,12 +87,22 @@ const go = async () => {
       "http://localhost:7478",
       "http://localhost:7479",
     ],
+    litNetwork: "custom",
     debug: true,
   });
   await litNodeClient.connect();
   const signatures = await litNodeClient.executeJs({
     code: litActionCode,
     authSig,
+    jsParams: {
+      aString: "meow",
+      anInt: 42,
+      aFloat: 123.456,
+      anArray: [1, 2, 3, 4],
+      anArrayOfStrings: ["a", "b", "c", "d"],
+      anObject: { x: 1, y: 2 },
+      anObjectOfStrings: { x: "a", y: "b" },
+    },
   });
   console.log("signatures: ", signatures);
 };

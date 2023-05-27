bd7903a489ed37f8fc65e0cdf6c3634dd63ce692
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-05-27 09:17:34 -0600

    add apple jwt example

--- js-sdkTests/authContext.js
+++ js-sdkTests/authContext.js
@@ -22,7 +22,7 @@ const authSig = {
 const runLitAction = async () => {
   const litNodeClient = new LitJsSdk.LitNodeClient({
     alertWhenUnauthorized: false,
-    litNetwork: "localhost",
+    litNetwork: "serrano",
     debug: true,
   });
   await litNodeClient.connect();
@@ -41,11 +41,17 @@ const runLitAction = async () => {
       //     "ya29.a0Aa4xrXMCyLStBQzLhC8il8YRPXIkEEgno9nB4PKvjCi6oIu-uIjeIoyfQoR99TcZf0IUMPfJfjRIJyIXtLk_kXLa5BmdUyJcJGP8SB4-UjlebOILidfItC8KR1sQR9LSFX55cw3_GTa5IqCOCTXME38z5ZMZaCgYKATASARASFQEjDvL9HinQH3Mk1UclCD011YbLfQ0163",
       //   authMethodType: 5,
       // },
+      // {
+      //   // google oauth JWT
+      //   accessToken:
+      //     "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3Y2MwZWY0YzcxODFjZjRjMGRjZWY3YjYwYWUyOGNjOTAyMmM3NmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDg5OTYwNTQyNzMzNjA1NjgxMzIiLCJlbWFpbCI6ImdldmVuc3RlZUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IlVYV1Z1eEJsdGswcEhKclllOEFXTUEiLCJpYXQiOjE2NjcxNjgyMTUsImV4cCI6MTY2NzE3MTgxNX0.ejZu5bADJ6cUsovV7otHAafy0mqWZBAtN860jvBdVe38XUi0v-eB5WWBPMD5zXcJxbXFvaPWCX8nTaE6S24cNNHJw0hq15irjRZeg9D2i7ToitR1LZSQ3rPCDQZPX4xYn7G-FH7C1DQ-7NEDMmr9ge4B6Qs4pT5Mj8ESVlA29yZjKCfk-zL7F5b6W0EOIA6G9rj6-3HgtazkHfIGHAtfBz4dqHjC4HJncHJzqIm9Y8eSBBnN-ZhYUr3cWxGCuFIw3yrGccv5_khfhbbk6TqdSeMO9YNWN3otiVB8Nwu2sb9VsllFoHIE0uGSzVZVbJgSK1GsGbJZe76ubLuObI5YFw",
+      //   authMethodType: 6,
+      // },
       {
-        // google oauth JWT
+        // apple oauth JWT
         accessToken:
-          "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3Y2MwZWY0YzcxODFjZjRjMGRjZWY3YjYwYWUyOGNjOTAyMmM3NmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDg5OTYwNTQyNzMzNjA1NjgxMzIiLCJlbWFpbCI6ImdldmVuc3RlZUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IlVYV1Z1eEJsdGswcEhKclllOEFXTUEiLCJpYXQiOjE2NjcxNjgyMTUsImV4cCI6MTY2NzE3MTgxNX0.ejZu5bADJ6cUsovV7otHAafy0mqWZBAtN860jvBdVe38XUi0v-eB5WWBPMD5zXcJxbXFvaPWCX8nTaE6S24cNNHJw0hq15irjRZeg9D2i7ToitR1LZSQ3rPCDQZPX4xYn7G-FH7C1DQ-7NEDMmr9ge4B6Qs4pT5Mj8ESVlA29yZjKCfk-zL7F5b6W0EOIA6G9rj6-3HgtazkHfIGHAtfBz4dqHjC4HJncHJzqIm9Y8eSBBnN-ZhYUr3cWxGCuFIw3yrGccv5_khfhbbk6TqdSeMO9YNWN3otiVB8Nwu2sb9VsllFoHIE0uGSzVZVbJgSK1GsGbJZe76ubLuObI5YFw",
-        authMethodType: 6,
+          "eyJraWQiOiJmaDZCczhDIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiaG9zdC5leHAuRXhwb25lbnQiLCJleHAiOjE2ODQzOTU2MDEsImlhdCI6MTY4NDMwOTIwMSwic3ViIjoiMDAxOTE0LjAxNDQ3MTM1OTgyOTQ5MWZhOTU3N2QwNzQ3NDMwMTcxLjEzNTAiLCJjX2hhc2giOiJIWXhDaHc4ZVpPUlV5eFFQelNpY3dRIiwiZW1haWwiOiJiYXB0aXN0ZS5ncmV2ZUBlc3BlbWUuY29tIiwiZW1haWxfdmVyaWZpZWQiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNjg0MzA5MjAxLCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.Sh94-NM0XayRqND_znbzVzxPsD4tCtFCAwubPwp__LBEfr4tNDrR6IQyhFshFn0UT2ScYAeaZlWcQOUuDWpleFX6eJ7WI7yBCywzHrW-cWIO_jivQ9tzd0F4xlstK15791xHe5YEPisA6O0TARYvhsdZ-fz4gwl3Fx9f1jKvGT39I01hYVwAYzKuyDJHDP_ma2phBH3gR7UeYDgA1fkmLb5_pcmisS81G3lCuN4oBeplwScWt0lFq6TwrjgPQhQX_kJU-MWe_u9vgvxSgBGWXuvZqRFjtpGvBdPWECqjG7R806Dqv407naawlOKoJnsuFXqSFLmJBbb2ajS7gDQ2Yw",
+        authMethodType: 8,
       },
     ],
     // all jsParams can be used anywhere in your litActionCode
